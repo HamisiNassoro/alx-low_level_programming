b56d770d7d37ab97ac7c6922e8551d08e96cf31b
Hamisi <hamsonassor14@gmail.com>
2022-09-05 07:44:59 -0700

    delete dnodeint at index

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 	new->next = *head;
 	if (*head != NULL)
-		(*head)->prev = new;
+		(dd_dnodeint.c*head)->prev = new;
 	*head = new;
 
 	return (new);
