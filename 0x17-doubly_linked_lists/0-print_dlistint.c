f23f856e32d0879e4c5c9609e27c5b78ba5ea4d9
Hamisi <hamsonassor14@gmail.com>
2022-09-05 06:58:26 -0700

    fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,13 +9,13 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t num_of_nodes = 0;
-	
+
 	while (h)
 	{
 		num_of_nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	
+
 	return (num_of_nodes);
 }
